a1dacadea5caa2e686b8e85b2e2ec1a6fe7c8ba0
Jorge Barroso <jorge_barroso_11@hotmail.com>
2015-04-09 03:47:53 +0200

    - After a couple of days, parsing is possible and submanga (submangasingle)   function, which was taken as test function, is been updated. It remains   necessary to parse for the image, but at least it is now able to find the   link to the next page for i < 10, to be able to do it for i >= 10 it's just   necessary to make buffer one unit higher. It will be done as soon as I start   coding again.

--- src/submanga.c
+++ src/submanga.c
@@ -3,6 +3,7 @@
 #include <curl/curl.h>
 #include <sys/stat.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 void submangasingle(char[], char[], char[], char[]);
 void submangabulk(char[], char[], char[], char[]);
@@ -40,90 +41,135 @@ void submanga(char url_orig[], char name[], char chapter[], char downdir[]){
 void submangasingle(char url_orig[], char name[], char chapter[], char downdir[]){
 	
 	FILE *fp;
+	FILE *img;
 CURL *curl;
 CURLcode res;
-short int i=1;
-//char url[100], p[4], file[40], file_orig[40];
-bool err = 0;
-//short int length = strlen(downdir);
+short int i=1, j=0, k=0;
+bool err = 0, pgfound = 0;
 char urldown[100] = "http://submanga.com/c";
+char pagedown[100];
 char baseimg[] = ".jpg";
 char tmpfile[] = "/tmp/.html";
-short unsigned int j=0, line = 1, result=0;
-unsigned int sz;
-char temp[512], p[3];
-char imgurl[7];
+short unsigned int result=0;
+char p[3], q[3], imgurl[7];
+char html[153600];
+int length;
 
 	strcat(urldown,(strtok(strrchr(url_orig, '/'), "\0")));
-	
 	while(err == 0){
+		if (i > 1)
+		result = pgfound = 0;
 		sprintf(p, "%d", i);
-		strcat(strcpy(imgurl, p), baseimg);
+		sprintf(q, "%d", k);
+		printf("%s %s\n", p, q);
+		strcat(strcpy(imgurl, q), baseimg);
+		puts(baseimg);
+		if (j==1)
+			strcat(urldown, "/");
+		if (j > 1 && j < 9 ){/* "i", and then p, higher than 9 so, two digits*/
+			length = strlen(urldown);
+			urldown[length-1] = '\0';
+		}
+		if (j > 0)
+			strcat(urldown, p);
+		printf("EEEEEEEEEEEEEY URLDOWN IS %s AND URL_ORIG IS %s\n", urldown, url_orig);
+		/* Download html page*/
 	if(curl) {
 		curl = curl_easy_init();
 		fp = fopen(tmpfile, "w+");
 		curl_easy_setopt(curl, CURLOPT_URL, url_orig);
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data_submanga); //Save
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);						//Where
+		curl_easy_setopt(curl, CURLOPT_URL, url_orig);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data_submanga); //Save
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);						//Where to save
 		curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);						//No output
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);						//Autoredirect
 	
-    /* Perform the request, res will get the return code */
+	
     res = curl_easy_perform(curl);
     fclose(fp);
-    /* Check for errors */
+    
     if(res != CURLE_OK){
-		fprintf(stderr, "Download failed: %s\n", curl_easy_strerror(res));
+		err = 1;
 		}
-    /* always cleanup */
+		
     curl_easy_cleanup(curl);
 	}
-	fseek(fp, 0L, SEEK_END);
-	sz = ftell(fp);
-	fseek(fp, 0L, SEEK_SET);
-	
-	sprintf(temp, "%d", sz);
-	
-	fp = fopen(tmpfile, "r");
-	
-	while(fgets(temp, sz, fp) != '\0'){
-			//printf("%s\n", temp);
-			if (strstr(temp,imgurl) != '\0'){
-				printf("Resultado: %s\n", temp);
-				result++;
-				}
-			line++;
-			}	
-			
-		line = 0;
-		result = 0;
+	/* HERE WILL START HTML PARSING OF THE FILE */			
 		
-    while(fgets(temp, 512, fp) != '\0'){
-			printf("%s\n", temp);
-			if (strstr(temp,urldown) != '\0'){
-				printf("Resultado: %s\n", temp);/*
-				fgets(temp, 32, fp);
-				strcpy(imgurl, temp);
-				puts(imgurl);*/
-				result++;
+		fp = fopen(tmpfile, "r");
+		/* Look for the next html page to download */
+		if (i < 10){
+		while (fgets(html, sizeof(html) - 1, fp) != '\0'){
+			puts(html);
+			if (strstr(html, urldown) != NULL){
+				printf("%s\n", strstr(html, urldown));
+				sscanf(strstr(html, urldown), "%30s", url_orig);
+				length = strlen(url_orig);
+				if (i == 1)
+					url_orig[length-1] = '\0';
+					printf("URL = %s\n", url_orig);
+					result++;
 				}
-			line++;
 			}
-			
+			strcpy(html,"");
+		}
+		
+		printf("%d\n", result);
+		if (result == 0)
+			err = 1;
+		
+		/* Look for the picture */
+		if (i < 10 && i>1){
+		while (fgets(html, sizeof(html) - 1, fp) != '\0'){
+			if (strstr(html, imgurl) != NULL){
+			//	if (strtok(strrchr('/'), '\0') > p){
+				printf("%s\n", strstr(html, imgurl));
+				sscanf(strstr(html, imgurl), "%30s", pagedown);
+				length = strlen(pagedown);
+				if (!isdigit(pagedown[length]))
+					pagedown[length-1] = '\0';
+					puts(pagedown);
+					pgfound = 1;
+				}
+			}
+			strcpy(html,"");
+		}
+		fclose(fp);
 		
 		printf("Downloading page %d...\n", i);
-		/*strcpy(file, file_orig);
-		strcpy(url,url_orig);*/
-		if (result == 0);
-		err = 1;
-    i++, j++;
+		printf("Downloading %s\n", url_orig);
+		
+		/* Now for the image */
+		if (pgfound == 1){
+		img = fopen(imgurl, "w+");
+		if(curl) {
+			printf("DOWNLOADING\n");
+		curl = curl_easy_init();
+		fp = fopen(tmpfile, "w+");
+		curl_easy_setopt(curl, CURLOPT_URL, pagedown);
+		curl_easy_setopt(curl, CURLOPT_URL, pagedown);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data_submanga); //Save
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, img);						//Where to save
+		curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);						//No output
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);					//Autoredirect
+	
+	
+		res = curl_easy_perform(curl);
+		fclose(img);
     
-    if (result == 0)
-		err = 1;
-    }
+		if(res != CURLE_OK){
+			err = 1;
+				}
+		
+		curl_easy_cleanup(curl);
+			}
+		}
+		i++, j++, k++;
+		
+	}
 	if (i > 2)
 		printf("%s chapter %s downloaded\n", name, chapter);
-	
+
 return;
 }
 
