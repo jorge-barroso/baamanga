96fe7e37a007312d1c9e623f14af6609e4da0383
jorge-barroso <jorge_barroso_11@hotmail.com>
2015-12-20 00:41:34 +0000

    Repaired downloads system according to new curlcpp statements

--- src/downloads.cpp
+++ src/downloads.cpp
@@ -5,13 +5,12 @@ void url_download(std::string url, std::string tmpfile, std::basic_fstream<char,
 {
     file.open(tmpfile);
     // Create a curl_ios object to handle the stream
-    curl::curl_ios<ostream> curl(tmpfile);
+    curl::curl_ios<ostream> curl(file);
     // Pass it to the easy constructor and watch the content returned in that file!
     curl::curl_easy easy(curl);
 
     easy.add<CURLOPT_URL>(url.c_str());
     easy.add<CURLOPT_FOLLOWLOCATION>(1L);
-    easy.add<CURLOPT_NOPROGRESS>(0L);
 
     try {
         easy.perform();
